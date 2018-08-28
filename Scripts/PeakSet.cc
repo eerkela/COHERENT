9ba10ede6cc02683d187ea60e5309b7bbff5fb6e
eerkela <38339616+eerkela@users.noreply.github.com>
2018-08-27 19:23:43 -0700

    Update PeakSet.cc

--- Scripts/PeakSet.cc
+++ Scripts/PeakSet.cc
@@ -1,90 +1,124 @@
-#include <TTree.h>
-#include <TCanvas.h>
-#include <TMath.h>
-#include <TH1.h>
-#include <TF1.h>
-#include <stdio.h>
-#include <algorithm>
-#include <TApplication.h>
-#include <TRint.h>
-#include <TGraph.h>
-#include <TGraphErrors.h>
-#include <TChain.h>
-#include <TMultiGraph.h>
-#include <TH2D.h>
-#include <TLine.h>
-#include <TSpectrum.h>
+#include <set>
 
 #include "CalStructs.h"
 #include "PeakSet.h"
 
-// This is a custom data structure emulating the behavior of a set of PeakInfo structs.  The set is
-// searchable by peak energy and will never contain two peaks with the same energy.
+/* This class describes a wrapper class for a set of PeakInfo structs, allowing the set to be
+searchable by peak energy.  It also allows the user to easily insert/update information stored in
+the set using the put method.
+*/
 
 PeakSet::PeakSet(std::vector<Double_t> energies) {
-	// Constructor: makes a new set of PeakInfo structs from a vector of energies.  All
-	// paramaters of the contained PeakInfo structs besides energy are initialized to null.
-	for(Int_t i = 0; i < energies.size(); i++) {
+/* Constructor: creates a new PeakSet from a vector of peak energies
+
+Accepts:
+	vector<Double_t> energies: a vector of peak energies to be included in the PeakSet.
+
+Returns:
+	a PeakSet containing PeakInfo structs with the supplied energies.  All parameters besides
+		the energy for each peak is initialized to null.
+
+*/
+	for(Double_t en : energies) {
 		PeakInfo curr;
-		curr.energy = energies[i];
-		this->put(curr);
+		curr.energy = en;
+		this->peaks.insert(curr);
 	}
 }
 
 PeakSet::PeakSet() {
-	// Standard Constructor: creates a PeakSet with no elements.
+/* default constructor: creates a PeakSet with no elements. */
 }
 
 void PeakSet::put(PeakInfo info) {
-	// puts a new PeakInfo struct into the set or updates it if it already exists
-	Int_t index = this->indexOf(info.energy);
-	if (index == -1) {
-		this->peakPars.push_back(info);
+/* puts a new peak into the set or updates it if it is already present
+
+Accepts:
+	PeakInfo info: the peak to be added to the set.  PeakInfo defined at line 12 of 
+		CalStructs.h
+
+*/
+	PeakInfo peak = this->get(info.energy);
+	if (peak.energy == -1) {
+		this->peaks.insert(info);
 	} else {
-		this->peakPars[index].mu = info.mu;
-		this->peakPars[index].muErr = info.muErr;
-		this->peakPars[index].sigma = info.sigma;
-		this->peakPars[index].sigmaErr = info.sigmaErr;
-		this->peakPars[index].count = info.count;
-		this->peakPars[index].includeInCal = info.includeInCal;
+		peak.mu = info.mu;
+		peak.muErr = info.muErr;
+		peak.sigma = info.sigma;
+		peak.sigmaErr = info.sigmaErr;
+		peak.count = info.count;
+		peak.includeInCal = info.includeInCal;
+		this->peaks.erase(peak);
+		this->peaks.insert(peak);
 	}
 }
 
 PeakInfo PeakSet::get(Double_t energy) {
-	// returns the PeakInfo struct in the set with the specified energy
-	Int_t index = this->indexOf(energy);
-	/*if (index == -1) {
-		throw invalid_argument("peak not in set: " + std::to_string(energy) + " keV");
-	}*/
-	return this->peakPars[index];
-}
+/* gets the peak stored in this PeakSet with the provided energy
 
-PeakInfo PeakSet::getAtIndex(Int_t index) {
-	return this->peakPars[index];
-}
+Accepts:
+	Double_t energy: the energy (in keV) of the peak to be retrieved
 
-PeakInfo PeakSet::getFirstPeak() {
-	// returns the PeakInfo for the highest energy peak.  This is useful because the highest
-	// energy peak must be pinned to allow for linear extrapolation of other peaks.
-	return this->peakPars[0];
-}
+Returns:
+	PeakInfo corresponding to the peak that stored in this PeakSet with the desired energy.
+		Returns a PeakInfo struct with energy = -1 if the set does not contain the peak.
 
-Int_t PeakSet::indexOf(Double_t energy) {
-	// returns index of the PeakInfo struct with the specified energy (returns -1 if not present)
-	for (Int_t i = 0; i < this->peakPars.size(); i++) {
-		PeakInfo curr = this->peakPars[i];
-		if (curr.energy == energy) {
-			return i;
+*/
+	for (PeakInfo pk : this->peaks) {
+		if (pk.energy == energy) {
+			return pk;
 		}
 	}
-	return -1;
+	// throw invalid_argument("peak not in set: " + std::to_string(energy) + " keV");
+	PeakInfo notFound;
+	notFound.energy = -1;
+	return notFound;
+}
+
+std::set<PeakInfo> PeakSet::getSet() {
+/* returns the actual set in which peaks are being stored
+
+TODO:
+	make PeakSet itself iterable so the user doesn't need to directly access the raw set.
+ 
+*/
+	return this->peaks;
+}
+
+PeakInfo PeakSet::remove(Double_t energy) {
+/* removes the peak with the provided energy from the set
+
+Accepts:
+	Double_t energy: the energy (in keV) of the peak to be removed.
+
+Returns:
+	PeakInfo describing the removed set.  If the set does not contain the peak, returns a
+		PeakInfo with energy = -1.
+
+*/
+	for (PeakInfo pk : this->peaks) {
+		if (pk.energy == energy) {
+			this->peaks.erase(pk);
+			return pk;
+		}
+	}
+	// throw invalid_argument("peak not in set: " + std::to_string(energy) + " keV");
+	PeakInfo notFound;
+	notFound.energy = -1;
+	return notFound;
 }
 
 bool PeakSet::contains(Double_t energy) {
-	// returns true if a PeakInfo struct exists in the set with the specified energy
-	return this->indexOf(energy) != -1;
+/* returns true if a PeakInfo struct exists in the set with the specified energy */
+	for (PeakInfo pk : this->peaks) {
+		if (pk.energy == energy) {
+			return true;
+		}
+	}
+	return false;
 }
 
 Int_t PeakSet::size() {
-	return this->peakPars.size();
+/* returns the current size of the set */
+	return this->peaks.size();
 }
