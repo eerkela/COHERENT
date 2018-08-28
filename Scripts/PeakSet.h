974f218a63d81c3d5710d7845cad3921f9973be2
eerkela <38339616+eerkela@users.noreply.github.com>
2018-08-27 19:24:04 -0700

    Update PeakSet.h

--- Scripts/PeakSet.h
+++ Scripts/PeakSet.h
@@ -5,15 +5,16 @@
 
 class PeakSet {
 private:
-	std::vector<PeakInfo> peakPars;
+	std::set<PeakInfo> peaks;
+	Double_t firstPeakEnergy;
 public:
 	PeakSet(std::vector<Double_t> energies);
 	PeakSet();
 	void put(PeakInfo info);
 	PeakInfo get(Double_t energy);
-	PeakInfo getAtIndex(Int_t index);
-	PeakInfo getHighestEnergyPeak();
-	Int_t indexOf(Double_t energy);
+	std::set<PeakInfo> getSet();
+	PeakInfo remove(Double_t energy);
+	PeakInfo getFirstPeak();
 	bool contains(Double_t energy);
 	Int_t size();
 };
